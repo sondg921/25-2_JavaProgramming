db8d92b3b15c3f1fd49de2ddd5d1dcd729c055a3
DongGeon <103568083+sondg921@users.noreply.github.com>
2025-11-13 14:41:25 +0900

    Add files via upload

---
+++ Homework/Homework7.java
@@ -0,0 +1,61 @@
+package Homework;
+
+abstract class Printer{
+    String model;
+    int printedCount;
+    int availableCount;
+
+    public Printer(String model, int availableCount){
+        this.model = model;
+        this.availableCount = availableCount;
+    }
+
+    public abstract boolean print();
+}
+
+class InkjetPrinter extends Printer{
+    public InkjetPrinter(String model, int availableCount){
+        super(model, availableCount);
+    }
+
+    @Override
+    public boolean print(){
+        if(availableCount == 0){
+            System.out.printf("%s: %d매째 인쇄 실패 - 잉크 부족.\n", model, printedCount+1);
+            return false;
+        }
+        else{
+            printedCount++;
+            availableCount--;
+            return true;
+        }
+    }
+}
+
+class LaserPrinter extends Printer{
+    public LaserPrinter(String model, int availableCount){
+        super(model, availableCount);
+    }
+
+    @Override
+    public boolean print(){
+        if(availableCount == 0){
+            System.out.printf("%s: %d매째 인쇄 실패 - 토너 부족.\n", model, printedCount+1);
+            return false;
+        }
+        else{
+            printedCount++;
+            availableCount--;
+            return true;
+        }
+    }
+}
+
+public class Homework7 {
+    public static void main(String[] args) {
+        InkjetPrinter inkjet = new InkjetPrinter("Brother DCP-T730DW", 7500);
+        LaserPrinter laser = new LaserPrinter("Canon MFC643Cdw", 1500);
+        while (inkjet.print());
+        while (laser.print());
+    }
+}
